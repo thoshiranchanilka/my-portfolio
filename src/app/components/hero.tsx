cc5b450b51e3eace81d4fae25536a0566ab9b07a
thoshiranchanilka <bgtch8356@gmail.com>
2023-02-28 01:31:18 +0530

    hero section update

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -5,59 +5,63 @@ import {
     Image,
     Stack,
     Text,
+    Box,
     useBreakpointValue,
+    useColorModeValue
   } from '@chakra-ui/react';
   
   export default function SplitScreen() {
     return (
-      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-        <Flex p={8} flex={1} align={'center'} justify={'center'}>
+      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }} mt={'10'}>
+        <Flex p={8} flex={1} align={'center'} justify={'center'} >
           <Stack spacing={6} w={'full'} maxW={'lg'}>
-            <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+            <Box marginLeft={{ base:10, md:20 }} mx={{ base: 'auto'}}>
+            <Heading>
               <Text
+                color={'gray.500'}
                 as={'span'}
                 position={'relative'}
-                _after={{
-                  content: "''",
-                  width: 'full',
-                  height: useBreakpointValue({ base: '20%', md: '30%' }),
-                  position: 'absolute',
-                  bottom: 1,
-                  left: 0,
-                  bg: 'blue.400',
-                  zIndex: -1,
-                }}>
-                Freelance
+                fontSize={{ base: 'xl', md: '2xl', lg: '3xl' }}
+                >
+                Hello! I'm
               </Text>
               <br />{' '}
-              <Text color={'blue.400'} as={'span'}>
-                Design Projects
+              <Text 
+                color={useColorModeValue('gray.800', 'gray.100')} 
+                as={'span'}
+                fontSize={{ base: '3xl', md: '5xl', lg: '5xl' }}
+                >
+                Thoshiran Chanilka
               </Text>{' '}
             </Heading>
             <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
-              The project board is an exclusive resource for contract work. It's
-              perfect for freelancers, agencies, and moonlighters.
+              A creative graphics designer and devoloper.
             </Text>
-            <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
-              <Button
-                rounded={'full'}
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}>
-                Create Project
-              </Button>
-              <Button rounded={'full'}>How It Works</Button>
-            </Stack>
+              <Box alignItems={'flex-start'}>
+                <Button
+                  mt={2}
+                  size={'sm'}
+                  px={'10'}
+                  rounded={'full'}
+                  bg={'gray.600'}
+                  color={'white'}
+                  _hover={{
+                   bg: 'gray.700',
+                 }}>
+                 Resume
+                </Button>
+              </Box>
+            </Box>
           </Stack>
         </Flex>
         <Flex flex={1}>
           <Image
-            alt={'Login Image'}
+            alt={'Thoshiran Chanilka'}
+            height={{base: '400px', md: '500px'}}
+            mx={{ base: 'auto', md: '1'}}
             objectFit={'cover'}
             src={
-              'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+              'https://images2.imgbox.com/30/3e/vqusIttK_o.png'
             }
           />
         </Flex>
