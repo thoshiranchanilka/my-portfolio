fe30d15b9809b4be1eca6814feadda1b20fb33d9
thoshiranchanilka <bgtch8356@gmail.com>
2023-02-27 11:42:25 +0530

    first commit

---
+++ src/app/components/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Thoshiran Chanilka</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="A profesional graphics designer" />
+        <link rel="icon" href="/Logo.svg" />
+      </>
+    )
+  }
