ad39c92755b2b2ba0216ed2006e114ba3768a419
Thoshiran Chanilka <124055408+thoshiranchanilka@users.noreply.github.com>
2023-03-21 10:54:07 +0530

    Update head.tsx

--- src/app/components/head.tsx
+++ src/app/components/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <title>Thoshiran Chanilka</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="A profesional graphics designer" />
+      <meta name="google-site-verification" content="EwZo3L6BAR_o-LhyxRZLtBOr5nHTjPBzOa5UE_Monjg" />
       <link rel="icon" type="image/x-icon" href="/logo-dark.svg" />
     </>
   );
